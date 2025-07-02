7174a8db77421866d2e98150fb2144c595b9d047
Xurify <52548195+Xurify@users.noreply.github.com>
2025-07-01 21:06:40 -0400

    Increased caching for flag images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // images: {
+  //   minimumCacheTTL: 86400 * 120,
+  //   formats: ["image/webp", "image/avif"],
+  // },
+  async headers() {
+    return [
+      {
+        source: '/images/flags/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ]
+  },
+
 };
 
 export default nextConfig;
