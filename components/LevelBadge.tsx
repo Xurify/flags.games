06e33c28ef42ba5b9080e17a69eb77b78322c087
Xurify <52548195+Xurify@users.noreply.github.com>
2025-08-19 13:41:44 -0400

    Updated colors in LevelBadge

--- components/LevelBadge.tsx
+++ components/LevelBadge.tsx
@@ -8,8 +8,8 @@ interface LevelBadgeProps {
 const badgeStyles: Record<string, string> = {
   easy: "bg-green-100 dark:bg-green-800 border-green-700 dark:border-green-600 text-green-700 dark:text-green-100",
   medium:
-    "bg-yellow-100 text-yellow-700 border-yellow-700 dark:bg-[#78632f] dark:text-[#ffe093] dark:border-yellow-600",
-  hard: "bg-orange-100 text-orange-700 border-orange-700 dark:bg-orange-400 dark:border-orange-700 dark:text-orange-50",
+    "bg-yellow-100 text-yellow-700 border-yellow-700 dark:bg-yellow-400 dark:border-yellow-900 dark:text-yellow-900",
+  hard: "bg-orange-100 text-orange-700 border-orange-700 dark:bg-orange-600 dark:border-orange-800 dark:text-orange-50",
   expert: "bg-red-100 text-red-700 border-red-700 dark:bg-red-800 dark:border-red-600 dark:text-red-100",
 };
 
