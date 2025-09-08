78c7715ff0404b959b228e3cefccd48aad9a977e
Xurify <52548195+Xurify@users.noreply.github.com>
2025-09-07 20:31:44 -0400

    Made FlagDisplay less rounded and added a slight border

--- components/FlagDisplay.tsx
+++ components/FlagDisplay.tsx
@@ -8,10 +8,10 @@ interface FlagDisplayProps {
 
 const FlagDisplay: React.FC<FlagDisplayProps> = ({ countryName, countryCode }) => {
   const isNepal = countryCode.trim().toLowerCase() === "np";
-  const flagClass = "max-w-full min-h-36 max-h-36 h-36 object-cover" + (isNepal ? "" : " rounded-sm");
+  const flagClass = "max-w-full min-h-36 max-h-36 h-36 object-cover" + (isNepal ? "" : " rounded border");
 
   return (
-    <div className="bg-muted/70 dark:bg-transparent rounded-2xl p-6 sm:p-10 flex justify-center items-center h-[192px] sm:h-[200px]">
+    <div className="bg-muted/70 dark:bg-transparent p-6 sm:p-10 flex justify-center items-center h-[192px] sm:h-[200px]">
       <Image
         src={`/images/flags/${countryCode.toLowerCase()}.svg`}
         alt={countryName ? `Flag of ${countryName}` : ""}
