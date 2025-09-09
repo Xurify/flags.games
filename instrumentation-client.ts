766bd410497496ce2e3d82ffc574a265c19ae5cf
Xurify <52548195+Xurify@users.noreply.github.com>
2025-09-09 01:55:43 -0400

    Removed Embrace.io

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,5 +1,4 @@
 import posthog from "posthog-js";
-import { initSDK } from "@embrace-io/web-sdk";
 import { vemetric } from '@vemetric/web';
 
 if (process.env.NODE_ENV !== "development") {
@@ -9,9 +8,6 @@ if (process.env.NODE_ENV !== "development") {
     defaults: "2025-05-24",
     capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
     //debug: process.env.NODE_ENV === "development",
-  });
-  initSDK({
-    appID: process.env.NEXT_PUBLIC_EMBRACE_APP_IDD!,
   });
   vemetric.init({
     token: process.env.NEXT_PUBLIC_VEMETRIC_TOKEN!,
