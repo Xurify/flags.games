b131e781541e4b6a9d351de6e768ff45b9390905
Xurify <52548195+Xurify@users.noreply.github.com>
2025-09-14 01:32:54 -0400

    Updated styles

--- components/GameEndScreen.tsx
+++ components/GameEndScreen.tsx
@@ -150,7 +150,7 @@ const GameEndScreen: React.FC<GameEndScreenProps> = ({
                   className={
                     result.isCorrect
                       ? ""
-                      : "bg-destructive/6 dark:bg-destructive/15 hover:bg-destructive/30 dark:hover:bg-destructive/20"
+                      : "bg-destructive/45 dark:bg-destructive/15 hover:bg-destructive/30 dark:hover:bg-destructive/20"
                   }
                 >
                   <TableCell className="text-right tabular-nums text-muted-foreground">
