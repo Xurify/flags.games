6bf0647d46ee541ea25f6b979b6bd902f00da6c5
Xurify <52548195+Xurify@users.noreply.github.com>
2025-12-18 10:14:37 -0500

    Removed unused code

--- app/lobby/[inviteCode]/layout.tsx
+++ app/lobby/[inviteCode]/layout.tsx
@@ -1,20 +1,12 @@
 "use client";
 
 import React from "react";
-import { Room } from "@/lib/types/socket";
-import { useGameState } from "@/lib/hooks/useGameState";
-import MultiplayerHeader from "@/components/multiplayer/MultiplayerHeader";
 
 export default function InviteCodeLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const { currentRoom } = useGameState();
-
-  const handleLeaveGame = () => {
-    window.location.href = "/lobby";
-  };
 
   return (
     <div className="min-h-screen w-full bg-transparent max-w-4xl mx-auto px-4 sm:px-6 py-4 sm:py-6">
